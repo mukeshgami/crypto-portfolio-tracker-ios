2a7aa8d04e66e93e9b4afca141e04397cb62bc89
mukeshgami <mukesh4only@gmail.com>
2025-09-08 19:56:33 +0530

    project started

---
+++ crypto-portfolio-tracker-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  crypto-portfolio-tracker-ios
+//
+//  Created by mukesh.gami on 08/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ crypto-portfolio-tracker-ios/crypto_portfolio_tracker_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  crypto_portfolio_tracker_iosApp.swift
+//  crypto-portfolio-tracker-ios
+//
+//  Created by mukesh.gami on 08/09/25.
+//
+
+import SwiftUI
+
+@main
+struct crypto_portfolio_tracker_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
